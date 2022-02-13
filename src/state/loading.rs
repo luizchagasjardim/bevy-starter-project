1e94d78c666916cd0fbc35c04fad0c03c914d331
Luiz Jardim <luizchagasjardim@gmail.com>
2022-02-13 18:55:13 +0000

    refactor

--- src/state/loading.rs
+++ src/state/loading.rs
@@ -96,28 +96,27 @@ pub fn setup_loading_bar(
 }
 
 fn load_textures(mut sprite_handles: ResMut<SpriteHandles>, asset_server: Res<AssetServer>) {
-    let mut load_impl = |name, start, number_of_frames| {
+    let mut load = |path, name, start, number_of_frames| {
         let handles = (start..start+number_of_frames)
-            .map(|i| format!["pixel-platformer/Characters/character_00{:0>2}.png", i])
-            .map(|file| asset_server.load(file.as_str()))
+            .map(|i| asset_server.load(format!["{}_{:0>4}.png", path, i].as_str()))
             .collect();
         sprite_handles.handles.insert(name, handles);
     };
-    //characters
-    load_impl("green", 0, 2);
-    load_impl("blue", 2, 2);
-    load_impl("pink", 4, 2);
-    load_impl("yellow", 6, 2);
-    load_impl("spike ball", 8, 1);
-    load_impl("beige", 9, 2);
-    load_impl("fish", 11, 2);
-    load_impl("block", 13, 2);
-    load_impl("hedgehog", 15, 3);
-    load_impl("baby jeremy", 18, 3);
-    load_impl("jeremy", 21, 3);
-    load_impl("angel", 24, 3);
-    //background
-    load_impl("angel", 24, 3);
+    let mut load_character = |name, start, number_of_frames| {
+        load("pixel-platformer/Characters/character", name, start, number_of_frames);
+    };
+    load_character("green", 0, 2);
+    load_character("blue", 2, 2);
+    load_character("pink", 4, 2);
+    load_character("yellow", 6, 2);
+    load_character("spike ball", 8, 1);
+    load_character("beige", 9, 2);
+    load_character("fish", 11, 2);
+    load_character("block", 13, 2);
+    load_character("hedgehog", 15, 3);
+    load_character("baby jeremy", 18, 3);
+    load_character("jeremy", 21, 3);
+    load_character("angel", 24, 3);
 }
 
 fn check_textures(
